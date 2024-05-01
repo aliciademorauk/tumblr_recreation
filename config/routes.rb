d80d2d5d161853b97902cd79182b36165a083955
Alicia de Mora Losana <aliciademoralosana@yahoo.com>
2024-05-01 17:36:19 +0100

    Add show view, route and action

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   get "/posts/new", to: "posts#new", as: "new_post"
   post "/posts", to: "posts#create"
-  
+  get "/posts/:id", to: "posts#show", as: "post"
+
   root "posts#index"
 end
