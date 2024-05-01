c6f0d03baed8f90a64a8116671bfe1adfcd63a7e
Alicia de Mora Losana <aliciademoralosana@yahoo.com>
2024-05-01 19:55:18 +0100

    Create partial dedicated to flash messages and render through application.html.erb; include flash messages styling

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,8 +10,10 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(post_params)
     if @post.save
+      flash[:notice] = "Post was successfully published!"
       redirect_to root_path
     else
+      flash[:alert] = "There was a problem creating the post. Please try again."
       render :new
     end
   end
@@ -21,7 +23,7 @@ class PostsController < ApplicationController
   end
 
   private
-  
+
   def post_params
     params.require(:post).permit(:title, :body)
   end
