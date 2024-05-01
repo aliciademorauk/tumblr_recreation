46480ae73e9b70523d5ab025930fdd1f0d521b6a
Alicia de Mora Losana <aliciademoralosana@yahoo.com>
2024-05-01 16:49:47 +0100

    Declare routing for new and create actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  get "/posts/new", to: "posts#new", as: "new_post"
+  post "/posts", to: "posts#create"
+  
   root "posts#index"
 end
