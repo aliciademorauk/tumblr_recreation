e66f58d49c798d9618beb296aa1d68b6904612d3
Alicia de Mora Losana <aliciademoralosana@yahoo.com>
2024-05-01 17:01:46 +0100

    Define index, new, create actions in controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,18 @@
 class PostsController < ApplicationController
-  
+  def index
+    @posts = Post.all.order(created_at: :desc)
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new
+    if @post.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
 end
