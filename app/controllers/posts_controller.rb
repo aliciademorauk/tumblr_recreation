f06ff9b4eddafad92abf2b3acd5bc1f5919161c0
Alicia de Mora Losana <aliciademoralosana@yahoo.com>
2024-05-02 12:00:18 +0100

    Add edit and update actions to controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,13 +13,31 @@ class PostsController < ApplicationController
       flash[:notice] = "Post was successfully published!"
       redirect_to root_path
     else
-      flash[:alert] = "There was a problem creating the post. Please try again."
+      flash.now[:error] = "There was a problem with your post. Please try again..."
       render :new
     end
   end
 
   def show
     @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      flash[:notice] = "Post was successfully updated!"
+      redirect_to root_path
+    else
+      flash.now[:error] = "There was a problem with your post. Please try again..."
+      render :edit
+    end
+  end
+
+  def destroy
   end
 
   private
