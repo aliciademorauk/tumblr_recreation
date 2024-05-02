695f7ab64bb9f3bdaa4431eb178cbea134c74a26
Alicia de Mora Losana <aliciademoralosana@yahoo.com>
2024-05-02 12:04:19 +0100

    Make route declarations for posts controller concise using call to resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  get "/posts/new", to: "posts#new", as: "new_post"
-  post "/posts", to: "posts#create"
-  get "/posts/:id", to: "posts#show", as: "post"
-
   root "posts#index"
+  resources :posts
 end
