5f1a178e6ee1161cf8fc3fc1837ec6b98fbe5d1a
Alicia de Mora Losana <aliciademoralosana@yahoo.com>
2024-05-03 16:16:06 +0100

    Create Comment model Create Comment model with a reference to the Post model and add association in Post model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_02_101310) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_03_142447) do
+  create_table "comments", force: :cascade do |t|
+    t.string "name"
+    t.text "body"
+    t.integer "post_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title", null: false
     t.text "body", null: false
@@ -18,4 +27,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_02_101310) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "posts"
 end
