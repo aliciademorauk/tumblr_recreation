00b2a212ea8421e93911521c1bd36985fc06e723
Alicia de Mora Losana <aliciademoralosana@yahoo.com>
2024-05-04 13:36:43 +0100

    Add new schema file

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_03_142447) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_04_121237) do
   create_table "comments", force: :cascade do |t|
-    t.string "name"
-    t.text "body"
+    t.text "body", null: false
     t.integer "post_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name", null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
   end
 
