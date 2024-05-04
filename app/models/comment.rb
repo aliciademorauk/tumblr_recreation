6bc43a30d60463e2aaade7bc5f5cc5c577d274a3
Alicia de Mora Losana <aliciademoralosana@yahoo.com>
2024-05-04 13:50:22 +0100

    Fix validation in Comment model, related to commit 78c26410c62f7029e49db5e77965276c2b269d4f

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,9 +1,8 @@
 class Comment < ApplicationRecord
   belongs_to :post
-  validates :title, 
+  validates :name, 
             presence: true, 
             length: { minimum: 5, maximum: 100 }
-
   validates :body, 
             presence: true, 
             length: { minimum: 30, maximum: 250 }
