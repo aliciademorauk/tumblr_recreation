9fb62e06c752d0d14100eddb8052e92c45e025e5
Alicia de Mora Losana <aliciademoralosana@yahoo.com>
2024-05-04 13:21:19 +0100

    Add validations to Comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,10 @@
 class Comment < ApplicationRecord
   belongs_to :post
+  validates :title, 
+            presence: true, 
+            length: { minimum: 5, maximum: 100 }
+
+  validates :body, 
+            presence: true, 
+            length: { minimum: 30, maximum: 250 }
 end
