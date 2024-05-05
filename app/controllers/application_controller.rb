58974217f596528bca48d9e43cb806e2ad7b5e3f
Alicia de Mora Losana <aliciademoralosana@yahoo.com>
2024-05-05 16:15:33 +0100

    Create sessions controller and session management helper methods
    - Add new create and destroy actions to sessions controller, downcasing user email before attempting to retrieve from the database
    - Add three methods for the application's controllers (and views)...
    --- 'current_user' retrieves the user if there is a user_id stored in the session and if @current_user is not already set
    --- 'authenticated' redirects the user to sign in page unless they are already signed in, to be used to restrict controller actions to signed in users and enforce server-side security
    --- 'user_owns_record?' returns whether the current user is the owner of the comment or post object it is called on, to be used to display options to modify comments or posts only to the owner

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user, :user_owns_record?
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+
+  def authenticated
+    unless current_user.present?
+      flash[:error] = "You must sign in first to perform this action."
+      redirect_to sign_in_path
+    end
+  end
+
+  def user_owns_record?(record)
+    current_user == record.user
+  end
 end
